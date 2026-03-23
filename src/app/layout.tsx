c697258b5fb8b1e5af721fd84810eb4efd51cf3b
srinivasgogula02 <gogulasrinivas261@gmail.com>
2026-03-23 15:47:57 +0530

    Integrate Meta Tracking Pixel (fbevents.js)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,8 +94,35 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '932694869167270');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={`${instrumentSans.variable} antialiased`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=932694869167270&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ClerkProvider>
           {children}
         </ClerkProvider>
